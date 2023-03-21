10f6167436d4ecaa2cea400b28684a291c224691
pragati1509 <77267328+pragati1509@users.noreply.github.com>
2023-03-22 00:54:15 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,13 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from '../store'
-import 'bootstrap/dist/css/bootstrap.css';
+
+// import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+
 import '../src/assets/main.css'
 
 const app = createApp(App)
 app.use(store);
 app.use(router)
-
+app.use(bootstrap);
 app.mount('#app');
+  
+  
+
   
   
